0a3dc9cd461d9aafb4172a98700d94eee659d97a
Tjark Rasche <tjark@rache.dev>
2020-09-06 00:34:02 +0200

    added doc comments to cluster.tf

--- infra/cluster.tf
+++ infra/cluster.tf
@@ -4,22 +4,20 @@ resource "digitalocean_kubernetes_cluster" "k8s" {
   region  = "fra1"
   # Grab the latest version slug from `doctl kubernetes options versions`
   version = "1.18.8-do.0"
-
   node_pool {
     name       = "base-load"
     size       = "s-1vcpu-1gb"
     node_count = 2
   }
 }
+# k8s namespace for all gateway/ingres related ressources
 resource "kubernetes_namespace" "gateway" {
   metadata {
-    
-
-    
-
     name = "gateway"
   }
 }
+
+# helm release for the gloo gateway
 resource "helm_release" "gateway" {
   name       = "gloo"
   repository = "https://storage.googleapis.com/solo-public-helm" 
